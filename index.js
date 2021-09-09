4b40ba88066d97d207a865481db77294d788e8e2
Jorgeluis <jorgeluis21.duran@gmail.com>
2021-09-09 11:51:01 -0400

    initializing project

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("express")()
+const { static } = require("express")
+const { resolve } = require("path")
+const { Server } = require("socket.io")
+const server = require('http').createServer(app)
+const io = new Server(server)
+const PORT = process.env.PORT || 4000
+const DOMAIN = 'http://localhost'
+
+io.on("connection", () => {
+    console.log("new connection")
+})
+
+app.use(static("public"))
+
+server.listen(PORT, () => console.log(`Server running on ${DOMAIN}:${PORT}`))
